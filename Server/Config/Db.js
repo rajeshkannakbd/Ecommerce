054c4ea96fa65a5a16ba968b1691c3c36796bf99
rajeshkanna <rajeshkannakbd@gmail.com>
2025-06-28 16:12:16 +0530

    mongodbAtlas

--- Server/Config/Db.js
+++ Server/Config/Db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/userDB");
+    await mongoose.connect("mongodb+srv://rajeshkannakbd:x1GyRTVCOdJAk1jA@cluster0.ykakrhi.mongodb.net/");
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection failed:", err);
